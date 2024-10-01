5e0654f65dd37f303768d1b1da22b977502a0b95
Tarek Benbrahim <140254531+heyitsmeyo@users.noreply.github.com>
2024-10-01 18:00:00 +0200

    Update model deployment.py

--- model deployment.py
+++ model deployment.py
@@ -18,7 +18,7 @@ le = load('/media/raspberry/64E5-D9E51/labelencoder81.pkl')
 engine = pyttsx3.init()
 
 # ESP32 IP address and port
-HOST = '192.168.175.165'
+HOST = ''
 PORT = 4444
 
 # Function to receive sensor data from ESP32
@@ -47,28 +47,25 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 gyro_dict = {'GYR{}'.format(i+1): gyro_value for i, gyro_value in enumerate(gyro)}
                 accel_dict = {'ACC{}'.format(i+1): accel_value for i, accel_value in enumerate(accel)}
 
-# Convert dictionaries to DataFrame
+
                 df_angles = pd.DataFrame([angles_dict])
                 df_gyro = pd.DataFrame([gyro_dict])
                 df_accel = pd.DataFrame([accel_dict])
 
-# Concatenate the three DataFrames along the columns axis
+
                 df = pd.concat([df_angles,  df_gyro ,  df_accel], axis=1)
             
                 df.columns = ['flex_1', 'flex_2', 'flex_3', 'flex_4', 'flex_5', 'GYRx', 'GYRy', 'GYRz', 'ACCx', 'ACCy', 'ACCz' ]
 
-# Scale the data using scaler.transform
 
 
 
            
-# Assuming accel_data and gyro_data are your accelerometer and gyroscope data
 
 
-                # Scale the features
+
+              
                 scaled_data = scaler.transform(df)
-
-                # Make prediction
                 predicted_label = le.inverse_transform(model.predict(scaled_data))
                 predicted_alphabet = predicted_label[0]
                 print(f"Predicted alphabet: {predicted_alphabet}")
